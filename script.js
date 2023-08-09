581ac27a7d9fafa6c834b790d2b5be2357739556
SavviSam <swghazzawi@gmail.com>
2023-08-09 03:17:30 -0600

    updates

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-let button = document.getElementsByTagName('button');
+let output = document.getElementById("output-value");
+let result = document.getElementById("output");
+let button = document.querySelectorAll("button");
 
+
+button.forEach((button) => {
+  button.addEventListener("click", function Input(num) {
+    
+    output.innerText += button.value;
+  });
+});

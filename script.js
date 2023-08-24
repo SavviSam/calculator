3d5ce80a21d046fb16f585f7ab63c5aae060f491
SavviSam <swghazzawi@gmail.com>
2023-08-24 12:36:59 -0600

    update features

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
-let output = document.getElementById("output-value");
-let result = document.getElementById("output");
-let button = document.querySelectorAll("button");
+const numberButton = document.querySelectorAll("[data-number]");
+const buttonStyle = document.querySelectorAll("button");
+const operationButton = document.querySelectorAll("[data-operation]");
+const output = document.querySelector("[data-output]");
+const equalsButton = document.querySelector("[data-equals]");
+const clearButton = document.querySelector("[data-clear]");
+const toggleButton = document.querySelector("[data-toggle]");
+const deleteButton = document.querySelector("[data-delete]");
 
-button.forEach((button) => {
-  const Input = () => {
-    output.innerText += button.value;
+//output displays value when user clicks on number buttons
+numberButton.forEach((button) => {
+  const buttonClick = () => {
+    output.append(button.textContent);
   };
 
-  button.addEventListener("click", Input);
+  //user keyboard events display on calculator
+  const keyBoard = (e) => {
+    switch (e.key) {
+      case "1":
+        console.log("hi");
+        break;
+    }
+  };
+
+  button.addEventListener("click", buttonClick);
+  window.addEventListener("keydown", (e) => {
+    switch (true) {
+      case e.key === button.textContent:
+        output.append(button.textContent);
+        break;
+    }
+  });
+});
+
+//reset the calculator display to zero
+clearButton.addEventListener("click", () => {
+  output.textContent = "";
 });
